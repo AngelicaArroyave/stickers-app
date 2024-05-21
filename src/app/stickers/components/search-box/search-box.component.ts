015cf6a660618f2d1a974cdfcb5a25cd96905a2a
DESKTOP-6N9G5LS\USUARIO <aarroyaveduque20@gmail.com>
2024-05-20 21:17:47 -0500

    Estilos a la página con el buscador y el sidebar, diseño base de la lista de stickers

---
+++ src/app/stickers/components/search-box/search-box.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stickers-search-box',
+  templateUrl: './search-box.component.html',
+})
+export class SearchBoxComponent {
+
+}
